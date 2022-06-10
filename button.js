c6a0408ae7fa7eccf0b29df37875c4e3aa2a19c4
ajaygupta001 <71979627+ajaygupta001@users.noreply.github.com>
2022-06-10 19:04:29 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,14 @@
+var data = 0;
+document.getElementById("root"), innerHTML = data;
+
+function decrement() {
+  data = data - 1;
+  document.getElementById("root").innerText = data;
+
+}
+
+function increment() {
+  data = data + 1;
+  document.getElementById("root").innerText = data;
+
+}
